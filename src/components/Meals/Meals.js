0dbc39892ba871528bebad61fa05a11c6f0bf882
Kumaran-project <kumaranselvaraj08@gmail.com>
2025-02-17 22:39:33 +0530

    added meal component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeal";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
