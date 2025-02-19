f6af4ef388f9765a29f942f35b0966c78c4d5e2b
Kumaran-project <kumaranselvaraj08@gmail.com>
2025-02-19 22:32:46 +0530

    added add ad minus functionality

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  const [amountIsvalid, setIsAmountValid] = useState(true);
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+  const amountInputRef = useRef();
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
+        ref={amountInputRef}
         input={{
           id: "amount",
           type: "number",
@@ -17,6 +37,7 @@ const MealItemForm = () => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsvalid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
